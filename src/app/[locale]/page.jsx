63ec1b5fe99ee2e084f8bc38b3bca37ecf5b0bd3
TomasSorgetti <tomassorgetti456@gmail.com>
2025-01-12 21:31:24 -0300

    feat: group classes responsive

--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -1,6 +1,7 @@
 import { getTranslations } from "next-intl/server";
 import HomeBanner from "@/layouts/home/Banner";
 import Pricing from "@/layouts/home/Pricing";
+import GroupClass from "@/layouts/home/GroupClass";
 
 export default async function HomePage() {
   const t = await getTranslations();
@@ -9,6 +10,7 @@ export default async function HomePage() {
     <main className="">
       <HomeBanner t={t} />
       <Pricing t={t} />
+      <GroupClass t={t} />
     </main>
   );
 }

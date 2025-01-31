e4909b0e58b85a3a64758affe220e719dfebf847
TomasSorgetti <tomassorgetti456@gmail.com>
2025-01-31 15:28:10 -0300

    feat: metadata settings

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -20,6 +20,62 @@ const Manrope = localFont({
   ],
 });
 
+export async function generateMetadata({ params, searchParams }, parent) {
+  const locale = (await params).locale;
+
+  const metadata = {
+    en: {
+      title:
+        "Learning Spark | Expert Online Tutors for IB and IGCSE Exams, Essays and Assesments",
+      description:
+        "Expert coaching for Extended Essays, Internal Assessments, and exam preparation in Mathematics, Biology, Physics, and more. Learn anywhere, anytime!",
+      keywords: "online tutors, IB, IGCSE, exams, essays, assessments",
+    },
+    es: {
+      title:
+        "Learning Spark | Tutores Online Expertos para Exámenes, Ensayos y Evaluaciones del IB e IGCSE",
+      description:
+        "Coaching experto para Ensayos Extendidos, Evaluaciones Internas y preparación de exámenes en Matemáticas, Biología, Física y más. ¡Aprende desde cualquier lugar, en cualquier momento!",
+      keywords: "tutores online, IB, IGCSE, exámenes, essays, assessments",
+    },
+  };
+
+  const selectedMetadata = metadata[locale] || metadata.en;
+
+  return {
+    title: selectedMetadata.title,
+    description: selectedMetadata.description,
+    openGraph: {
+      type: "website",
+      locale: locale === "en" ? "en_US" : "es_ES",
+      title: selectedMetadata.title || metadata.en.title,
+      description: selectedMetadata.description || metadata.en.description,
+      url: `https://learning-spark.com/${locale}`,
+      images: [
+        {
+          url: `https://learning-spark.com/metadata/opengraph.png`,
+          width: 1200,
+          height: 630,
+          alt: "A preview of the Learning Spark website",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: selectedMetadata.title,
+      description: selectedMetadata.description,
+      images: [`https://learning-spark.com/metadata/opengraph.png`],
+    },
+    alternates: {
+      canonical: `https://learning-spark.com/${locale}`,
+      languages: {
+        en: "https://learning-spark.com/en",
+        es: "https://learning-spark.com/es",
+      },
+    },
+  };
+}
+
 export default async function LocaleLayout({ children, params }) {
   const locale = (await params).locale;
 
--- src/app/[locale]/page.jsx
+++ src/app/[locale]/page.jsx
@@ -6,7 +6,35 @@ import Contact from "@/layouts/home/Contact";
 import Coaching from "@/layouts/home/Coaching";
 import WhatsappButton from "@/components/buttons/WhatsappButton";
 
-export default async function HomePage() {
+export async function generateMetadata({ params, searchParams }, parent) {
+  const locale = (await params).locale;
+
+  const metadata = {
+    en: {
+      title:
+        "Learning Spark | Expert Online Tutors for IB and IGCSE Exams, Essays and Assesments",
+      description:
+        "Expert coaching for Extended Essays, Internal Assessments, and exam preparation in Mathematics, Biology, Physics, and more. Learn anywhere, anytime!",
+      keywords: "online tutors, IB, IGCSE, exams, essays, assessments",
+    },
+    es: {
+      title:
+        "Learning Spark | Tutores Online Expertos para Exámenes, Ensayos y Evaluaciones del IB e IGCSE",
+      description:
+        "Coaching experto para Ensayos Extendidos, Evaluaciones Internas y preparación de exámenes en Matemáticas, Biología, Física y más. ¡Aprende desde cualquier lugar, en cualquier momento!",
+      keywords: "tutores online, IB, IGCSE, exámenes, essays, assessments",
+    },
+  };
+
+  const selectedMetadata = metadata[locale] || metadata.en;
+
+  return {
+    title: selectedMetadata.title,
+    description: selectedMetadata.description,
+  };
+}
+
+export default async function HomePage({ params, searchParams }) {
   const t = await getTranslations();
 
   return (

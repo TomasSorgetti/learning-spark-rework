77a29e8fa65b6efc7087eda3f4d99b3a5b4e7903
TomasSorgetti <tomassorgetti456@gmail.com>
2025-01-31 16:57:33 -0300

    feat: about translations & semantic tags

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const constants = {
   whatsapp: "(+54 9) 11-3947-8794",
   whatsappLink: "https://api.whatsapp.com/send?phone=5491139478794",
-  email: "learningspark@gmail.com",
+  email: "andythomsenhall@gmail.com",
 };
--- src/layouts/home/About.jsx
+++ src/layouts/home/About.jsx
@@ -9,7 +9,7 @@ export default function About({ t }) {
       <Image
         className="w-full h-auto lg:w-auto lg:max-w-[50%]"
         src="/images/about/about.png"
-        alt={t("HomePage.About.title")}
+        alt="teacher and student in class"
         width={604}
         height={505}
         draggable="false"
@@ -20,15 +20,22 @@ export default function About({ t }) {
         <span className="text-[14px] text-subTitleColor font-medium">
           {t("HomePage.About.span")}
         </span>
-        <h3 className="font-poppins text-[26px] mb-5 font-extrabold text-left text-titleColor xl:text-[2.188rem]">
+        <h2 className="font-poppins text-[26px] mb-5 font-extrabold text-left text-titleColor xl:text-[2.188rem]">
           {t("HomePage.About.title")}
-        </h3>
+        </h2>
         <div className="flex flex-col gap-2 mt-6 leading-7 lg:gap-8 lg:mt-10">
           <p className="text-[1rem] font-normal text-textColor xl:text-[1.063rem]">
-            {t("HomePage.About.description1")}
+            {t("HomePage.About.description1")}{" "}
+            <strong className="font-bold">
+              {t("HomePage.About.description1span")}
+            </strong>
           </p>
           <p className="text-[16px] font-normal text-textColor">
-            {t("HomePage.About.description2")}
+            {t("HomePage.About.description2")}{" "}
+            <strong className="font-bold">
+              {t("HomePage.About.description2span")}{" "}
+            </strong>
+            {t("HomePage.About.description2End")}
           </p>
         </div>
       </article>
